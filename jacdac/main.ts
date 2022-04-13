1db1bef082137480815e44b8177951a52aa18cd7
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-12 19:00:50 -0700

    Update jacdac/main.ts, jacdac/test.ts

---
+++ jacdac/main.ts
@@ -0,0 +1,66 @@
+//% deprecated
+namespace makerbit { }
+
+namespace modules {
+    /**
+     * Maker:bit motor 1
+     */
+    //% fixedInstance whenUsed block="makerbit motor A"
+    export const makerbitMotorA = new MotorClient("makerbit motor A?device=self")
+    /**
+     * Maker:bit motor 2
+     */
+    //% fixedInstance whenUsed block="makerbit motor B"
+    export const makerbitMotorB = new MotorClient("makerbit motor B?device=self")
+}
+
+namespace servers {
+    class MotorServer extends jacdac.Server {
+        duty = 0
+        enabled = false
+        motor: MakerBitMotor
+        constructor(motor: MakerBitMotor) {
+            super(jacdac.SRV_MOTOR)
+            this.motor = motor;
+            this.syncDuty()
+        }
+
+        private syncDuty() {
+            if (this.duty === 0 || isNaN(this.duty) || !this.enabled) {
+                makerbit.stopMotor(this.motor)
+            } else {
+                makerbit.setMotorRotation(
+                    this.motor,
+                    this.duty >= 0 ? MakerBitMotorRotation.Forward : MakerBitMotorRotation.Backward
+                )
+                const speed = Math.abs(this.duty) * 100
+                makerbit.runMotor(this.motor, speed)
+            }
+        }
+
+        handlePacket(pkt: jacdac.JDPacket) {
+            this.handleRegBool(pkt, jacdac.MotorReg.Reversible, true)
+
+            const oldEnabled = this.enabled
+            const oldDuty = this.duty
+
+            this.enabled = this.handleRegBool(pkt,
+                jacdac.MotorReg.Enabled, this.enabled)
+            this.duty = this.handleRegValue(pkt,
+                jacdac.MotorReg.Duty,
+                jacdac.MotorRegPack.Duty,
+                oldDuty)
+
+            this.syncDuty()
+        }
+    }
+
+    function start() {
+        jacdac.productIdentifier = 0x3cadc101
+        jacdac.startSelfServers(() => [
+            new MotorServer(MakerBitMotor.A),
+            new MotorServer(MakerBitMotor.B),
+        ])
+    }
+    start()
+}
--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,1 +1,17 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let speed = 0
+let dv = 5
+forever(() => {
+    modules.makerbitMotorA.setEnabled(true)
+    modules.makerbitMotorB.setEnabled(true)
+
+    modules.makerbitMotorA.setDuty(speed)
+    modules.makerbitMotorB.setDuty(1 - speed)
+
+    speed += dv
+    if (speed > 100) {
+        dv = -5
+    } else if (speed < -100) {
+        dv = 5
+    }
+    pause(250)
+})
